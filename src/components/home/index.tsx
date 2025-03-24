382b0846ddf99d5f7d48b555691bb32d84453590
Ramnetsystem <ramtecnicos@ramsystem.com.br>
2025-03-24 14:35:34 -0300

    changes

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -9,16 +9,16 @@ const Home = () => {
   };
 
   return (
-    <section className="relative h-screen bg-[url('../src/assets/bg.jpg')] bg-cover bg-center bg-no-repeat flex items-center justify-center overflow-hidden">
+    <section className="relative h-screen bg-[url('../src/assets/bg.jpg')] bg-cover bg-center bg-no-repeat flex items-center justify-center overflow-hidden" id='home'>
       <div className="absolute inset-0 bg-black opacity-80"></div>
       
       {/* Conteúdo */}
       <Container>
         <section className="relative z-10 flex items-center justify-center w-full h-screen flex-col gap-20">
           <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-4xl sm:text-6xl font-bold text-center text-white">SLOGAN AQUI</h1>
-            <h1 className="text-4xl sm:text-6xl font-bold text-center text-custom-orange">SLOGAN AQUI</h1>
-            <h1 className="text-4xl sm:text-6xl font-bold text-center text-white">SLOGAN AQUI</h1>
+            <h1 className="text-4xl sm:text-6xl font-bold text-center text-white">Inteligência</h1>
+            <h1 className="text-4xl sm:text-6xl font-bold text-center text-custom-orange">Eficiência</h1>
+            <h1 className="text-4xl sm:text-6xl font-bold text-center text-white">Precisão</h1>
           </div>
 
           {/* Botão que rola para About */}
--- src/components/menu_list/index.tsx
+++ src/components/menu_list/index.tsx
@@ -1,6 +1,9 @@
 
 const MenuList = () => {
 
+  const scrollToHome = () => {
+    document.getElementById("home")?.scrollIntoView({ behavior: "smooth" });
+  };
 
     const scrollToAbout = () => {
         document.getElementById("about")?.scrollIntoView({ behavior: "smooth" });
@@ -21,7 +24,7 @@ const MenuList = () => {
 
     return (
         <ul className="flex flex-col items-center justify-center gap-6 sm:flex-row p-2 font-bold text-2xl sm:text-base">
-            <li className="cursor-pointer hover:text-orange-900">Pagina Principal</li>
+            <li className="cursor-pointer hover:text-orange-900" onClick={scrollToHome}>Pagina Principal</li>
             <li className="cursor-pointer hover:text-orange-900" onClick={scrollToAbout}>Sobre Nós </li>
             <li className="cursor-pointer hover:text-orange-900" onClick={scrollToServices}>Serviços</li>
             <li className="cursor-pointer hover:text-orange-900" onClick={scrollToClients}>Clientes</li>
